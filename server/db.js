bfca394077ecc18dff221d623ed17b38fea148a3
Mirr2 <mirr2.jo@gmail.com>
2024-06-17 00:17:44 +0900

    MODIFY::

--- server/db.js
+++ server/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 const connection = mysql.createPool({
   host     : 'localhost',
   user     : 'root',
-  password : 'ckadnjf77',
+  password : 'pw',
   database : 'solve_this_db'
 });
 
